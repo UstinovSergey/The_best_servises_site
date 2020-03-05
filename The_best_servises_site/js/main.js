fb3f98586fcef79abd0ce020d14f0617b1320a9e
UstinovSergey <56814109+UstinovSergey@users.noreply.github.com>
2020-03-05 11:34:14 +0800

    Add files via upload

---
+++ The_best_servises_site/js/main.js
@@ -0,0 +1,27 @@
+$(function () {
+  $('.header-slider',).slick({
+
+    
+    // dots: true,
+    vertical: true,
+    // dotsClass: "header-dots",
+    autoplay: 2000,
+ 
+
+  })
+  $('.slider-servises-content').slick({
+
+  
+  })
+  
+   $('.fedback-slider__description').slick({
+   
+ 
+  })
+
+  // $('.fedback-slider__photos').slick({
+  //   arrows:false,
+
+  // })
+});
+
